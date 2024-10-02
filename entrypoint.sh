ac4d8290cad6309913dd69316f86ba285c28c84d
Matt Sowden <msowden@consultaegis.com>
2024-10-02 17:11:26 -0400

    Rm unnessesary build step for the moment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 if [ ! -f ./config/settings.ini ]; then
   echo "Could not find the config file, assuming this is a new installation and launching the setup server."
-
-  cd setup/ui
-  npm run integrate
-  cd ../../
-
   hypercorn setup.app:asgi_app -b 0.0.0.0:8080
 else
   echo "Running..."
